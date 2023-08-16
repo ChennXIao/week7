7e9ea52e798728d9e682c26791a0330885c3d631
ChennXIao <61040179+ChennXIao@users.noreply.github.com>
2023-08-16 12:18:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,18 +50,6 @@ def ver():
         cnt.commit()
         return render_template("index.html")
 
-
-# @app.route('/signin',methods=["POST"])
-# def mem():
-#     account = request.form.get("account", "")
-#     password = request.form.get("password", "")
-
-#     session["account"] = account
-#     session["password"] = password
-
-#     return redirect(url_for('member'))
-
-
 @app.route("/api/member")
 def api_member():
     
@@ -80,8 +68,6 @@ def api_member():
         
     return jsonify(response)
 
-
-
 @app.route("/api/member", methods = ["PATCH"])
 def update_member():
     
@@ -97,7 +83,6 @@ def update_member():
 
     return jsonify(response)
 
-
 @app.route("/member")
 def member():
 
@@ -110,8 +95,6 @@ def member():
         return render_template("member.html", user=name)
     else:
         return redirect(url_for('index'))
-
-
 
 @app.route('/signin',methods=["POST"])
 def mem():
@@ -132,8 +115,6 @@ def mem():
     else:
         return redirect("/error?message=請輸入正確的帳號密碼")
 
-
-
 @app.route("/error")
 def error():
     message = request.args.get("message", "")
@@ -145,94 +126,3 @@ def out():
     return redirect(url_for('index'))
 app.debug = True
 app.run()
-
-
-# @app.route('/signin',methods=["POST"])
-# def mem():
-#     account = request.form.get("account", "")
-#     password = request.form.get("password", "")
-#     signin = ("SELECT name, username, password, id FROM member WHERE username= %s and password = %s LIMIT 1;")
-#     cur.execute(signin,(account,password))
-#     member = cur.fetchone()
-    
-#     if member !=None:
-        
-#         session["id"] = member["id"]
-#         session["name"] = member["name"]
-#         session["account"] = member["username"]
-#         session["password"] = member["password"]
-#         print(session)
-#         url_mem = f"/api/member?username={session['name']}" 
-
-#         return redirect(url_mem)
-#     else:
-#         return redirect("/error?message=請輸入正確的帳號密碼")
-
-
-
-# @app.route("/member")
-# def member():
-#     cur.execute("SELECT name, member_id,content FROM member INNER JOIN message ON member.id = message.member_id ORDER BY message.time DESC;")
-#     data = cur.fetchall()
-
-#     if "name" in session:
-#         name = session["name"]
-#         print(name)
-#         return render_template("member.html", user=name,text=data)
-#     else:
-#         return redirect(url_for('index'))
-
-# @app.route("/createMessage",methods=["POST"])
-# def message():
-
-#     id = session["id"]  
-#     text = request.form.get("text","")
-
-#     message = (id,text)
-#     add_message = ("INSERT INTO message(member_id,content)VALUES(%s,%s);")
-#     cur.execute(add_message,message)
-#     cnt.commit()
-#     return redirect(url_for('member'))
-
-# @app.route("/deleteMessage",methods=["POST"])
-# def me():
-#     id = session["id"] 
-#     del_message = (("DELETE FROM message WHERE member_id ='{}';").format(id))
-#     cur.execute(del_message)
-#     print("delete")
-#     cnt.commit()
-#     return redirect(url_for('member'))
-
-# @app.route('/deleteMessage', methods=['POST'])
-# def delete_message():
-#     if request.is_json:
-#         data = json.loads(request.data.decode('utf-8'))
-#         message_id = data.get('message_id')
-
-#         # Delete the message from the database
-#         # Your code to connect to MySQL and delete the message
-
-#         response_data = {'success': True}
-#         return jsonify(response_data)
-#     else:
-#         print(request.is_json)
-#         # Return a response indicating that the content type is not supported
-#         response_data = {'error': 'Unsupported Media Type'}
-#         return jsonify(response_data), 415  # 415 is the HTTP status code for Unsupported Media Type
-    
-# @app.route('/deleteMessage/<int:message_id>', methods=['GET', 'POST'])
-# def delete_message(message_id):
-#     try:
-#         del_message = "DELETE FROM message WHERE member_id = %s"
-#         cur.execute(del_message, (message_id,))
-#         print("delete")
-#         cnt.commit()
-
-#         response = {'success': True}
-#     except Exception as e:
-#         cnt.rollback()
-#         response = {'success': False, 'error': str(e)}
-    
-#     return jsonify(response)
-
-
